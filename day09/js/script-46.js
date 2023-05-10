1e263595cbdf8c739269d86530fa96fb71ea05e4
User1 <sevenmuratt@gmail.com>
2023-05-10 21:51:50 +0300

    day09-1

--- day09/js/script-46.js
+++ day09/js/script-46.js
@@ -3,32 +3,24 @@ const txtScore2 = document.querySelector("#txtScore2");
 const txtScore3 = document.querySelector("#txtScore3");
 const btnAverage = document.querySelector("#btnAverage");
 const txtResult = document.querySelector("#txtResult");
-
 btnAverage.addEventListener("click", ()=>{
     const score1 = txtScore1.value;
     const score2 = txtScore2.value;
     const score3 = txtScore3.value;
-
     if(!isScoreValid(score1) || !isScoreValid(score2) || !isScoreValid(score3)){
         alert("Please enter valid scores");
         return;
     }
-
     const average = getAverage(score1, score2, score3);
     const letter = convertScoreToLetter(average);
     txtResult.innerHTML = `Average is ${average.toFixed(0)} (${letter})`;
 });
-
-
-
 const isScoreValid = (score) => { 
-    return (score || score === 0) && !isNaN(score) && score<=100 && score>=0;
+    return score && !isNaN(score) && score<=100 && score>=0;
 }
-
 const getAverage = (num1, num2, num3) => { 
     return (Number(num1)+Number(num2)+Number(num3))/3;
 }
-
 const convertScoreToLetter = (score) => { 
     let letter = "";
     if(score>=90 && score<=100){
