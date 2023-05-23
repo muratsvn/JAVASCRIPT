38304018e634fdf0823af1073ba2aac717606af0
User1 <sevenmuratt@gmail.com>
2023-05-23 22:43:36 +0300

    day20-1

--- day20/js/script-106.js
+++ day20/js/script-106.js
@@ -1,43 +1,51 @@
 import { searchShows } from "./tvmaze-api.js";
-
 let timeoutSearch = null;
-document.getElementById("txtSearch").addEventListener("input", (e)=>{
-    const query = e.target.value;
-    if(timeoutSearch) clearTimeout(timeoutSearch);
-    timeoutSearch = setTimeout(()=>{
-        searchShows(query, (shows)=>{
-            createMovies(shows)
-        });
-    }, 500);
-    
+const lstTvShows = document.getElementById("lstTvShows");
 
+
+document.getElementById("txtSearch").addEventListener("input", (e) => {
+  const query = e.target.value;
+  if (timeoutSearch) clearTimeout(timeoutSearch);
+  timeoutSearch = setTimeout(() => {
+    searchShows(query, (shows) => {
+      createMovies(shows);
+    });
+  }, 500);
 });
-const createMovies = (shows) =>{
-    const lstTvShows = document.getElementById("lstTvShows");
 
-    lstTvShows.innerHTML = "";
-    shows.forEach( (item)=> {
-        const movieCard = createMovieCard(item);
-        lstTvShows.insertAdjacentHTML("afterbegin",movieCard);
-    })
-}
+
+lstTvShows.addEventListener("click", (e)=>{
+  const selectedCard = e.target.closest(".card");
+  const showId = selectedCard.dataset.show;
+  location.href = `107-tvshow-details.html?id=${showId}`;
+});
+
+
+const createMovies = (shows) => {
+  lstTvShows.innerHTML = "";
+  shows.forEach((item) => {
+    const movieCard = createMovieCard(item);
+    lstTvShows.insertAdjacentHTML("afterbegin", movieCard);
+  });
+};
+
 
 const createMovieCard = (item) => {
-    const {image,name,genres } = item.show;
+  const { id, image, name, genres } = item.show;
 
-    let movieImage = "img/noimage.png";
-    if(image){
-        movieImage = image.medium;
-    } 
+  let movieImage = "img/noimage.png";
+  if (image) {
+    movieImage = image.medium;
+  }
 
-    return `
+  return `
     <div class="col">
-        <div class="card h-100">
-        <img src=${movieImage}} class="card-img-top" alt="${name}" />
-        <div class="card-body">
-            <h5 class="card-title">${name}</h5>
-            <p class="card-text">${genres.join("-")}</p>
+        <div class="card h-100" style="cursor:pointer" data-show="${id}">
+          <img src=${movieImage} class="card-img-top" alt="${name}" />
+          <div class="card-body">
+              <h5 class="card-title">${name}</h5>
+              <p class="card-text">${genres.join("-")}</p>
+          </div>
         </div>
-        </div>
-    </div>`
-}
+    </div>`;
+};
--- day20/js/tvmaze-api.js
+++ day20/js/tvmaze-api.js
@@ -1,13 +1,14 @@
 const baseUrl = "https://api.tvmaze.com";
-
 const searchShows = (query, cb) => {
     const url = `${baseUrl}/search/shows?q=${query}`;
     fetch(url)
     .then( (resp) => resp.json() )
     .then( (data) => cb(data) )
 }
-
-const getShowDetails = () =>{
+const getShowDetails = (showId, cb) =>{
+    const url = `${baseUrl}/shows/${showId}`;
+    fetch(url)
+    .then( (resp) => resp.json() )
+    .then( (data) => cb(data) )
 }
-
 export { searchShows, getShowDetails };
